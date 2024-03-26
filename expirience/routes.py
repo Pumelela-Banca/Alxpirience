2e2a259dfb2846262b5337f7d5e5c5928d581ced
Pumelela-Banca <pumelelabanca@gmail.com>
2024-03-27 01:35:35 +0200

    added correct import paths for data_base

--- expirience/routes.py
+++ expirience/routes.py
@@ -5,7 +5,8 @@ Runns app and routes
 from flask import render_template, url_for, flash, redirect
 from .home.log_in import RegisterForm, LoginForm
 
-from expirience import app
+from expirience import app, db, bcrypt
+from .data_base import User, Skills, Projects
 
 
 @app.route('/')
@@ -23,6 +24,10 @@ def register():
     """
     form = RegisterForm()
     if form.validate_on_submit():
+        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+        user = User(username=form.username.data, email=form.email.data, country=form.country.data, password=hashed_password)
+        db.session.add(user)
+        db.session.commit()
         flash(f'Account created for {form.username.data}!', 'success')
         return redirect(url_for('home'))
     return render_template('register.html', title="Register", form=form)
