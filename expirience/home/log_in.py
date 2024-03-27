f64af3181edef214fd15452a5521fc925bd09e3d
Pumelela-Banca <pumelelabanca@gmail.com>
2024-03-27 19:11:22 +0200

    added routes for job creation, profile views and job forms

--- expirience/home/log_in.py
+++ expirience/home/log_in.py
@@ -2,11 +2,9 @@
 log-in page details
 """
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, TextAreaField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from expirience.data_base import User
-
-
 
 
 class RegisterForm(FlaskForm):
@@ -39,9 +37,7 @@ class RegisterForm(FlaskForm):
         user = User.query.filter_by(email=email.data).first()
         if user:
             raise ValidationError("Email is taken. Please choose a different one.")
-    
-    
-         
+
 
 class LoginForm(FlaskForm):
     """
@@ -53,3 +49,46 @@ class LoginForm(FlaskForm):
                              validators=[DataRequired()])
     remember = BooleanField('Remember Me')
     submit = SubmitField('Log in')
+
+
+class UserUpdateForm(FlaskForm):
+    """
+    controls user update form
+    """
+    username = StringField('Username',
+                           validators=[DataRequired(), Length(min=2, max=20)])
+    email = StringField('Email',
+                           validators=[DataRequired(), Email()])
+    country = StringField('Country',
+                           validators=[DataRequired()])
+    #skills = StringField('Skills')
+    submit = SubmitField('Update')
+
+    def validate_field(self, field):
+        """
+        validates user name uniquesness
+        """
+        user = User.query.filter_by(username=field.data).first()
+        if user:
+            raise ValidationError("Username is taken. Please choose a different one.")
+        
+    def validate_email(self, email):
+        """
+        validates email uniquesness
+        """
+        user = User.query.filter_by(email=email.data).first()
+        if user:
+            raise ValidationError("Email is taken. Please choose a different one.")
+
+
+class CreateJobForm(FlaskForm):
+    """
+    controls job creation form
+    """
+    job_title = StringField('Job Title',
+                           validators=[DataRequired()])
+    job_description = TextAreaField('Job Description',
+                                    validators=[DataRequired()])
+    job_link = StringField('Job Link/Git-Hub Link',
+                           validators=[DataRequired()])
+    submit = SubmitField('Create Job')
--- expirience/routes.py
+++ expirience/routes.py
@@ -3,7 +3,7 @@ Runns app and routes
 """
 
 from flask import render_template, url_for, flash, redirect
-from .home.log_in import RegisterForm, LoginForm
+from .home.log_in import RegisterForm, LoginForm, UserUpdateForm, CreateJobForm
 from flask_login import login_user, current_user, logout_user, login_required
 
 from expirience import app, db, bcrypt
@@ -25,6 +25,7 @@ def register():
     """
     if current_user.is_authenticated:
         return redirect(url_for('home'))
+    
     form = RegisterForm()
     if form.validate_on_submit():
         hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
@@ -53,3 +54,91 @@ def login():
         else:
             flash('Login Unsuccessful. Please check username and password', 'danger')
     return render_template('login.html', title="Login" , form=form)
+
+
+@app.route('/logout')
+def logout():
+    """
+    logs out user
+    """
+    logout_user()
+    return redirect(url_for('home'))
+
+@app.route('/profile')
+def profile():
+    """
+    renders profile page
+    """
+    if not current_user.is_authenticated:
+        return redirect(url_for('home'))
+    pass
+
+@app.route('/editprofile', methods=['GET', 'POST'])
+def editprofile():
+    """
+    renders edit profile page
+    """
+    if not current_user.is_authenticated:
+        return redirect(url_for('home'))
+    
+    form = UserUpdateForm()
+    if form.validate_on_submit():
+        current_user.username = form.username.data
+        current_user.email = form.email.data
+        current_user.country = form.country.data
+        db.session.commit()
+        flash('Your account has been updated!', 'success')
+        return redirect(url_for('profile'))
+
+@app.route('/addskills', methods=['GET', 'POST'])
+def addskills():
+    """
+    renders add skills page
+    """
+    if not current_user.is_authenticated:
+        return redirect(url_for('home'))
+    
+
+@app.route('/jobs')
+def jobs():
+    """
+    renders jobs page
+    all other peoples jobs skips user
+    """
+    # to do
+    return render_template('jobs.html', title="Jobs")
+
+
+@app.route('/projects')
+def projects():
+    """
+    route to all projects page
+    """
+    pass
+
+@app.route('/myprojects', methods=['GET', 'POST'])
+def myprojects():
+    """
+    route to user projects page that can be edited
+    """
+    if not current_user.is_authenticated:
+        return redirect(url_for('home'))
+    
+    pass
+
+@app.route('/createproject', methods=['GET', 'POST'])
+def createproject():
+    """
+    route to create project page
+    """
+    if not current_user.is_authenticated:
+        return redirect(url_for('home'))
+    
+    form = CreateJobForm()
+    if form.validate_on_submit():
+        project = Projects(project_name=form.project_name.data, project_description=form.project_description.data, project_link=form.project_link.data)
+        db.session.add(project)
+        db.session.commit()
+        flash(f'Project created for {form.project_name.data}!', 'success')
+        return redirect(url_for('home'))
+    return render_template('createproject.html', title="Create Project", form=form)
