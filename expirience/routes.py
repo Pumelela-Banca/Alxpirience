0f8c30e7785b20230760582a424ad581fb42f62c
Pumelela-Banca <pumelelabanca@gmail.com>
2024-03-27 02:00:21 +0200

    added login authentication and session for user

--- expirience/routes.py
+++ expirience/routes.py
@@ -4,6 +4,7 @@ Runns app and routes
 
 from flask import render_template, url_for, flash, redirect
 from .home.log_in import RegisterForm, LoginForm
+from flask_login import login_user, current_user, logout_user, login_required
 
 from expirience import app, db, bcrypt
 from .data_base import User, Skills, Projects
@@ -22,6 +23,8 @@ def register():
     """
     renders register page
     """
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
     form = RegisterForm()
     if form.validate_on_submit():
         hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
@@ -35,7 +38,18 @@ def register():
 @app.route('/login')
 def login():
     """
-    renders login page
+    renders login page and hadles validation
     """
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+
     form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user and bcrypt.check_password_hash(user.password, form.password.data):
+            flash('Login Successful', 'success')
+            login_user(user, remember=form.remember.data)
+            return redirect(url_for('home'))
+        else:
+            flash('Login Unsuccessful. Please check username and password', 'danger')
     return render_template('login.html', title="Login" , form=form)
