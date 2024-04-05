fab74fc1e7bcecf6380489e5e2f071aaf2a30005
Pumelela-Banca <pumelelabanca@gmail.com>
2024-04-05 20:01:09 +0200

    added if statements to collecting skills

--- expirience/routes.py
+++ expirience/routes.py
@@ -117,11 +117,27 @@ def addskills():
     form = SkillsForm()
     if form.validate_on_submit():
         for value in form.data.values():
-            if value == 'Submit' or value == None:
-                continue
-            skill = Skills(skill=value, author=current_user)
-            db.session.add(skill)
-            db.session.commit()
+            for val, ky in form.data.items():
+                if ky == "skill":
+                    skill = Skills(skill=val, author=current_user)
+                    db.session.add(skill)
+                    db.session.commit()
+                elif ky == "skill_1":
+                    skill = Skills(skill_1=val, author=current_user)
+                    db.session.add(skill)
+                    db.session.commit()
+                elif ky == "skill_2":
+                    skill = Skills(skill_2=val, author=current_user)
+                    db.session.add(skill)
+                    db.session.commit()
+                elif ky == "skill_3":
+                    skill = Skills(skill_3=val, author=current_user)
+                    db.session.add(skill)
+                    db.session.commit()
+                elif ky == "skill_4":
+                    skill = Skills(skill_4=val, author=current_user)
+                    db.session.add(skill)
+                    db.session.commit()
         flash('Skills added!', 'success')
         return redirect(url_for('home'))
     return render_template('addskills.html', title="Add Skills", form=form)
